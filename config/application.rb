2305053ff186e0e753326fec770eb4241250219f
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-02-21 16:14:09 +0900

    fix rspec config

--- config/application.rb
+++ config/application.rb
@@ -42,3 +42,18 @@ module MyTodoApp
     config.api_only = true
   end
 end
+
+module Projects
+  class Application < Rails::Application
+    config.load_defaults 7.1
+    config.autoload_lib(ignore: %w(assets tasks))
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      g.factory_bot false
+    end
+  end
+end
